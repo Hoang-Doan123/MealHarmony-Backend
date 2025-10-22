e1431964bf41c92aa19561c3402a34d323c48ac8
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2025-10-22 12:27:24 +0700

    First commit

---
+++ src/main/java/com/example/MealHarmony/MealHarmonyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MealHarmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealHarmonyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealHarmonyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MealHarmony/MealHarmonyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MealHarmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealHarmonyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
